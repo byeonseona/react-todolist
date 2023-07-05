ada706085d496afe704a7c5fa2c242f881f8a1f7
byeonseona <sunah0128@gmail.com>
2023-07-05 09:49:53 +0900

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,57 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import styled from "styled-components";
+import { IoSunnyOutline, IoMoonSharp } from "react-icons/io5";
+import { ThemeProvider } from "styled-components";
+import { DarkTheme, LightTheme } from "./theme";
 
-function App() {
+const Header = styled.div`
+  height: 60px;
+  display: flex;
+  padding: 0px 20px;
+  justify-content: space-between;
+  align-items: center;
+  border-bottom: 1px solid #ddd;
+  background: ${({ theme }) => theme.color.background};
+  color: ${({ theme }) => theme.color.point};
+  cursor : pointer;
+`;
+
+const Body = styled.div`
+  min-height: 500px;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  background: ${({ theme }) => theme.color.background};
+  color: ${({ theme }) => theme.color.point};
+  cursor : pointer;
+`;
+
+
+
+export default function App() {
+
+  const [darkMode, setDarkMode] = React.useState(false);
+  const darkModeHandler = (e) => {
+    setDarkMode(!darkMode);
+  };
+  
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <ThemeProvider theme={darkMode ? DarkTheme : LightTheme}>
+        <Header>
+          <>다크 모드</>
+          <>
+            {darkMode ? (
+              <IoMoonSharp size={24} onClick={darkModeHandler} />
+            ) : (
+              <IoSunnyOutline size={24} onClick={darkModeHandler} />
+            )}
+          </>
+        </Header>
+        <Body>
+          <h1>Dark Mode</h1>
+        </Body>
+      </ThemeProvider>
+    </>
   );
 }
-
-export default App;
---
+++ src/theme.js
@@ -0,0 +1,13 @@
+export const LightTheme = {
+  color: {
+    background: "#FAFAFA",
+    point: "#0072CE"
+  }
+};
+
+export const DarkTheme = {
+  color: {
+    background: "#2C2D33",
+    point: "#fff"
+  }
+};
